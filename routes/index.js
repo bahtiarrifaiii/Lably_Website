cf9f14d33921dde0dd270a1b023ab22f78feb11e
fkriahmadar <fikriuser87@gmail.com>
2025-11-25 12:11:24 +0700

    order-completed page

--- routes/index.js
+++ routes/index.js
@@ -129,7 +129,7 @@ router.get("/customer", (req, res) => {
 });
 
 // ==========================
-// CUSTOMERS PAGE
+// ORDER PAGE
 // ==========================
 router.get("/order", (req, res) => {
   if (!req.session.admin) return res.redirect("/login");
@@ -149,6 +149,37 @@ router.get("/order", (req, res) => {
           style: `
             <link rel="stylesheet" href="/css/sidebar.css">
             <link rel="stylesheet" href="/css/order.css">
+          `,
+          content,
+          message: null,
+          showPopup: false
+        });
+      }
+    );
+  });
+});
+
+// ==========================
+// ORDER COMPLETED PAGE
+// ==========================
+router.get("/order-completed", (req, res) => {
+  if (!req.session.admin) return res.redirect("/login");
+
+  User.getAll((err, users) => {
+    if (err) throw err;
+
+    const totalCustomers = users.length;
+
+    ejs.renderFile(
+      path.join(__dirname, "../views/pages/admin/order/order_completed.ejs"),
+      { users, totalCustomers },
+      (err, content) => {
+        res.render("layouts/atmin", {
+          title: "Orders | Lably",
+          meta: "",
+          style: `
+            <link rel="stylesheet" href="/css/sidebar.css">
+            <link rel="stylesheet" href="/css/order_completed.css">
           `,
           content,
           message: null,
